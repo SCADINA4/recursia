ee3e3ac1ebcc13b85c43fc4cef9edd7717b29040
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-03-29 13:20:05 +0300

    Добавьте файлы проекта.

---
+++ recursia/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using recursia.ViewsModels;
+
+namespace recursia.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainWindowViewModel).Qwe(long.Parse(numberTextBox.Text));
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+    }
+}
---
+++ recursia/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using recursia.ViewsModels.Base;
+
+
+namespace recursia.ViewsModels
+{
+    public class MainWindowViewModel:ViewModel
+    {
+        private long factorial;
+
+        public long Factorial
+        {
+            get { return factorial; }
+            set { Set(ref factorial, value); }
+        }
+
+        public long Qwe(long n)
+        {
+            if (n == 1) 
+                return 1;
+
+            return Factorial = n * Qwe(Convert.ToInt64((n - 1)));
+        }
+    }
+}
